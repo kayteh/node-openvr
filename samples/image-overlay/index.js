3f17704d7b612669a7e8100c551de839b88135a8
Katalina <git@kat.cafe>
2018-08-23 07:21:10 -0500

    sample/image-overlay: adjust sample to possibly render

--- samples/image-overlay/index.js
+++ samples/image-overlay/index.js
@@ -1,7 +1,7 @@
 const vr = require('../../index.js')
 const vrTools = require('../../tools')
 const VROverlay = require('../../overlay')
-const path = require('path')
+// const path = require('path')
 
 class ImageOverlay {
   constructor () {
@@ -15,15 +15,20 @@ class ImageOverlay {
     for (let y = 0; y < h; y++) {
       const colorBase = y / h
       for (let x = 0; x < w; x++) {
-        out.push(colorBase, colorBase, colorBase, 1)
+        out.push(
+          (Math.cos(colorBase * h / x) + 0.33) % 1,
+          (Math.cos(colorBase * h / x) + 0.66) % 1,
+          Math.cos(colorBase * h / x),
+          1
+        )
       }
     }
 
-    // throw it out as the typedarray it always wanted to be. 
+    // throw it out as the typedarray it always wanted to be.
     // and scale it up because our math is based on 0-1 www
     console.log('done generating')
-    // return Uint8ClampedArray.from(out.map(x => x * 255))
-    return Float32Array.from(out)
+    return Uint8ClampedArray.from(out.map(x => x * 255))
+    // return Float32Array.from(out)
   }
 
   init () {
@@ -32,11 +37,9 @@ class ImageOverlay {
 
     this.overlay = new VROverlay({ system: this.system, key: 'electronvr.sample' })
     // this.overlay.width = 1
-    this.overlay.transformTrackedDeviceRelative(vr.k_unTrackedDeviceIndex_Hmd, { x: 0, y: 0, z: 1 })
+    this.overlay.transformTrackedDeviceRelative(vr.k_unTrackedDeviceIndex_Hmd, { x: 0, y: 0, z: -1 })
 
-    this.overlay.setTextureFromFile(path.join(__dirname, 'sample.jpg'))
-
-    const size = 256
+    const size = 1024
     this.overlay.setTextureFromBuffer(this.generateGradient(size, size), { width: size, height: size })
     console.log('set texture done')
 
